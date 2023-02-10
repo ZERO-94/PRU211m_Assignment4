73308d8ebe8c716c0e602d817a4a1733f1afae3e
trongnhandev1011 <82007391+trongnhandev1011@users.noreply.github.com>
2023-02-10 21:50:20 +0700

    feat: add goal

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
 
     public bool isDead = false;
+    public bool isWin = false;
 
     [SerializeField]
     private Rigidbody2D rb;
@@ -69,6 +70,10 @@ public class PlayerMovement : MonoBehaviour
         if(col.gameObject.tag == "Enemy")
         {
             isDead = true;
+        }
+        if(col.gameObject.tag == "Goal")
+        {
+            isWin = true;
         }
     }
 
