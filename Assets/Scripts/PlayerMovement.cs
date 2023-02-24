088ba8e7d337de03046aa49c08aada1051217f90
taiducvng <taiducvng17@gmail.com>
2023-02-24 22:01:41 +0700

    Update the start _restart button
    Update the start _restart button and some UI changes for production.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool isDead = false;
     public bool isWin = false;
-
+    public bool isHardMode = false;
     [SerializeField]
     private Rigidbody2D rb;
     [SerializeField]
@@ -30,12 +30,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsCollidedDeadZone()) isDead = true;
+        if (IsCollidedDeadZone()) 
+            isDead = true    ;
+        
 
         if (isDead)
         {
-            //ignore
-            return;
+            SceneManager.LoadScene("GameOverScene");
+            return ;
         }
 
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -70,8 +72,9 @@ public class PlayerMovement : MonoBehaviour
         if(col.gameObject.tag == "Enemy")
         {
             isDead = true;
+
         }
-        if(col.gameObject.tag == "Goal")
+        if (col.gameObject.tag == "Goal")
         {
             isWin = true;
         }
@@ -80,6 +83,7 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groudCheck.position, 0.2f, groudLayer);
+
     }
 
     private bool IsCollidedDeadZone()
